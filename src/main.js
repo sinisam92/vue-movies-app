354192143af8db71960f9166cded128772014379
Sinisa Manojlovic <sinisamanojlovic1@gmail.com>
2018-12-16 21:28:57 +0100

    Zamenjen api

--- src/main.js
+++ src/main.js
@@ -2,20 +2,16 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import AppMovies from './components/AppMovies.vue';
-import MoviesRow from './components/MoviesRow.vue';
-import MovieSearch from './components/MovieSearch.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
- window.EventBus = new Vue();
-
+window.EventBus = new Vue();
 
 const routes = [
   { path: '/', redirect: '/movies' },
   { path: '/movies', component: AppMovies, name: 'movies' }
-  
 ];
 
 const router = new VueRouter({
@@ -25,5 +21,5 @@ const router = new VueRouter({
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
--- src/service/Movies.js
+++ src/service/Movies.js
@@ -1,15 +1,14 @@
-import axios from 'axios'
+import axios from 'axios';
 
 class Movies {
-    constructor() {
-        axios.defaults.baseURL = 'http://localhost:3000/api/';
-    }
-    getAll() {
-        return axios.get('movies');
-    }
-
+  constructor() {
+    axios.defaults.baseURL = 'http://localhost:8000/api/';
+  }
+  getAll() {
+    return axios.get('movies');
+  }
 }
 
 const movies = new Movies();
 
-export default movies
+export default movies;
